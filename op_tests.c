8e40a6d14ad0700a710d70d7ed64a66a93973667
pebraz <pbraz.93@gmail.com>
2016-01-27 22:46:46 +0000

    fixed tests

--- op_tests.c
+++ op_tests.c
@@ -12,7 +12,9 @@
 #define INTERRUPT_SHOULD_BE_SET 8
 #define OVERFLOW_SHOULD_NOT_BE_SET 9
 #define OVERFLOW_SHOULD_BE_SET 10
-#define WRONG_VALUE 11
+#define DECIMAL_FLAG_SHOULD_NOT_BE_SET 11
+#define DECIMAL_FLAG_SHOULD_BE_SET 12
+#define WRONG_VALUE 13
 
 #define TEST(test) \
     int e_##test = test();\
@@ -27,8 +29,8 @@ int test_immediate() {
     immediate(&Nes, lda);
     
     if (Nes.regs.acc != 0x22)   return WRONG_VALUE;
-    if (Nes.regs.status & ZERO_FLAG != ZERO_FLAG)   return ZERO_SHOULD_NOT_BE_SET;
-    if (Nes.regs.status & NEGATIVE_FLAG != NEGATIVE_FLAG) return SIGN_SHOULD_NOT_BE_SET;
+    if (Nes.regs.status & ZERO_FLAG)   return ZERO_SHOULD_NOT_BE_SET;
+    if (Nes.regs.status & NEGATIVE_FLAG) return SIGN_SHOULD_NOT_BE_SET;
 
     return 0;
 
@@ -48,33 +50,23 @@ int test_absolute()
     return 0;
 
 }
-/*
 
-int test_absolute_sta() {
-
-    struct nestor nes;
-
-    nes.regs.acc = 45;
-    absolute(&nes, sta, 0x3F43); // store 45 in memory position
-    
-    if (nes.memory[0x3F43] != 45)   return -1;
-    return 0;
-
-}
 
 int test_adc() {
 
     struct nestor nes;
     nes = (struct nestor){.regs={0}};
 
-    immediate(&nes, adc, 45);   //add 45 to acc
+    nes.regs.pc = 0x0600;
+    nes.memory[0x0601] = 45;
+    immediate(&nes, adc);   
 
-    if (nes.regs.acc != 45) return -1;
-    if (nes.regs.status & ZERO_FLAG) return -1;
-    if (nes.regs.status & CARRY_FLAG) return -1;
-    if (nes.regs.status & NEGATIVE_FLAG) return -1; //failing here
-    if (nes.regs.status & OVERFLOW_FLAG) return -1;
-    if (nes.regs.status & DECIMAL_FLAG) return -1;
+    if (nes.regs.acc != 45) return WRONG_VALUE;
+    if (nes.regs.status & ZERO_FLAG) return ZERO_SHOULD_NOT_BE_SET;
+    if (nes.regs.status & CARRY_FLAG) return CARRY_SHOULD_NOT_BE_SET;
+    if (nes.regs.status & NEGATIVE_FLAG) return SIGN_SHOULD_NOT_BE_SET; 
+    if (nes.regs.status & OVERFLOW_FLAG) return OVERFLOW_SHOULD_NOT_BE_SET;
+    if (nes.regs.status & DECIMAL_FLAG) return DECIMAL_FLAG_SHOULD_NOT_BE_SET;
 
     return 0;
 }
@@ -83,14 +75,17 @@ int test_adc_zero()
 {
     struct nestor nes;
     nes = (struct nestor){.regs={0}};
-    immediate(&nes, adc, 0);    
 
-    if (!(nes.regs.status & ZERO_FLAG)) return -1;
+    nes.regs.pc = 0x0600;
+    nes.memory[0x0601] = 0;
+    immediate(&nes, adc);    
 
-    if (nes.regs.acc != 0) return -1;
-    if (nes.regs.status & CARRY_FLAG) return -1;
-    if (nes.regs.status & NEGATIVE_FLAG) return -1;
-    if (nes.regs.status & OVERFLOW_FLAG) return -1;
+    if (!(nes.regs.status & ZERO_FLAG)) return ZERO_SHOULD_BE_SET;
+
+    if (nes.regs.acc != 0) return WRONG_VALUE;
+    if (nes.regs.status & CARRY_FLAG) return CARRY_SHOULD_NOT_BE_SET;
+    if (nes.regs.status & NEGATIVE_FLAG) return SIGN_SHOULD_NOT_BE_SET;
+    if (nes.regs.status & OVERFLOW_FLAG) return OVERFLOW_SHOULD_NOT_BE_SET;
 
     return 0;
 }
@@ -101,14 +96,17 @@ int test_adc_negative()
     //negative
     nes = (struct nestor){.regs={0}};
     nes.regs.acc = 128;     // -1 (1000)
-    immediate(&nes, adc, 1);    // -2 (1001)
 
-    if (!(nes.regs.status & NEGATIVE_FLAG)) return -1;
+    nes.regs.pc = 0x0600;
+    nes.memory[0x0601] = 1; // -2 (1001)
+    immediate(&nes, adc);    
 
-    if (nes.regs.acc != 129) return -1;
-    if (nes.regs.status & CARRY_FLAG) return -1;
-    if (nes.regs.status & ZERO_FLAG) return -1;
-    if (nes.regs.status & OVERFLOW_FLAG) return -1;
+    if (!(nes.regs.status & NEGATIVE_FLAG)) return SIGN_SHOULD_BE_SET;
+
+    if (nes.regs.acc != 129) return WRONG_VALUE;
+    if (nes.regs.status & CARRY_FLAG) return CARRY_SHOULD_NOT_BE_SET;
+    if (nes.regs.status & ZERO_FLAG) return ZERO_SHOULD_NOT_BE_SET;
+    if (nes.regs.status & OVERFLOW_FLAG) return OVERFLOW_SHOULD_NOT_BE_SET;
 
     return 0;
 }
@@ -118,15 +116,19 @@ int test_adc_overflow()
     struct nestor nes;
     //over +127 overflow
     nes = (struct nestor){.regs={0}};
-    nes.regs.acc = 127;     
-    immediate(&nes, adc, 1);    // 128 -> -1 (1000)
+    nes.regs.acc = 127;   
 
-    if (!(nes.regs.status & NEGATIVE_FLAG)) return -1;
-    if (!(nes.regs.status & OVERFLOW_FLAG)) return -1;
+    nes.regs.pc = 0x0600;
+    nes.memory[0x0601] = 1;   
 
-    if (nes.regs.acc != 128) return -1;
-    if (nes.regs.status & CARRY_FLAG) return -1;
-    if (nes.regs.status & ZERO_FLAG) return -1;
+    immediate(&nes, adc);    // 128 -> -1 (1000)
+
+    if (!(nes.regs.status & NEGATIVE_FLAG)) return SIGN_SHOULD_BE_SET;
+    if (!(nes.regs.status & OVERFLOW_FLAG)) return OVERFLOW_SHOULD_BE_SET;
+
+    if (nes.regs.acc != 128) return WRONG_VALUE;
+    if (nes.regs.status & CARRY_FLAG) return CARRY_SHOULD_NOT_BE_SET;
+    if (nes.regs.status & ZERO_FLAG) return ZERO_SHOULD_NOT_BE_SET;
 
     return 0;
 }
@@ -136,96 +138,149 @@ int test_adc_carry()
     struct nestor nes;
     // carry 
     nes = (struct nestor){.regs={0}};
-    nes.regs.acc = 255; //-128
-    immediate(&nes, adc, 1); // 0
 
-    if (!(nes.regs.status & CARRY_FLAG)) return -1;
-    if (!(nes.regs.status & ZERO_FLAG)) return -1;
+    nes.regs.acc = 255; //-128
+    nes.regs.pc = 0x0600;
+    nes.memory[0x0601] = 1;  
+
+    immediate(&nes, adc); // 0
+
+    if (!(nes.regs.status & CARRY_FLAG)) return CARRY_SHOULD_BE_SET;
+    if (!(nes.regs.status & ZERO_FLAG)) return ZERO_SHOULD_BE_SET;
 
     if (nes.regs.acc != 0) return -1;
-    if (nes.regs.status & NEGATIVE_FLAG) return -1;
-    if (nes.regs.status & OVERFLOW_FLAG) return -1;
+    if (nes.regs.status & NEGATIVE_FLAG) return SIGN_SHOULD_NOT_BE_SET;
+    if (nes.regs.status & OVERFLOW_FLAG) return OVERFLOW_SHOULD_NOT_BE_SET;
 
     return 0;
 }
 
-int test_and()
+int test_and_1()
 {
     struct nestor nes;
 
     nes = (struct nestor){.regs={0}};
     nes.regs.acc = 0;   //-128
-    immediate(&nes, and, 0); // 0
+    nes.regs.pc = 0x0600;
+    nes.memory[0x0601] = 0;  
 
-    if (!(nes.regs.status & ZERO_FLAG)) return -1;
-    if (nes.regs.acc != 0) return -1;
-    if (nes.regs.status & NEGATIVE_FLAG) return -1;
+    immediate(&nes, and); // 0
+
+    if (!(nes.regs.status & ZERO_FLAG)) return ZERO_SHOULD_BE_SET;
+    if (nes.regs.acc != 0) return WRONG_VALUE;
+    if (nes.regs.status & NEGATIVE_FLAG) return SIGN_SHOULD_NOT_BE_SET;
+    
+    return 0;
+}
+
+int test_and_2()
+{
+    struct nestor nes;
+    nes = (struct nestor){.regs={0}};
 
     nes.regs.acc = 0x80;
-    immediate(&nes, and, 0x80);
+    nes.regs.pc = 0x0600;
+    nes.memory[0x0601] = 0xFF;
 
-    if (nes.regs.status & ZERO_FLAG) return -1;
-    if (nes.regs.acc != 0x80) return -1;
-    if (!(nes.regs.status & NEGATIVE_FLAG)) return -1;
+    immediate(&nes, and);
+
+    if (nes.regs.status & ZERO_FLAG) return ZERO_SHOULD_NOT_BE_SET;
+    if (nes.regs.acc != 0x80) return WRONG_VALUE;
+    if (!(nes.regs.status & NEGATIVE_FLAG)) return SIGN_SHOULD_BE_SET;
 
 
     return 0;
 
 }
 
-int test_asl()
+int test_asl_1()
 {
     struct nestor nes = (struct nestor){.regs={0}};
 
     nes.regs.acc = 0xFF;
     accumulator(&nes, asl);
 
-    if (nes.regs.acc != 0xFE) return -1;
-    if (!(nes.regs.status & CARRY_FLAG)) return -1;
+    if (nes.regs.acc != 0xFE) return WRONG_VALUE;
+    if (!(nes.regs.status & CARRY_FLAG)) return ;
     if (!(nes.regs.status & NEGATIVE_FLAG)) return -1;
     if (nes.regs.status & ZERO_FLAG) return -1;
+    
+    return 0;
+}
+
+int test_asl_2()
+{
+    struct nestor nes = (struct nestor){.regs={0}};
 
     nes.regs.acc = 0;
     accumulator(&nes, asl);
 
-    if (!(nes.regs.status & ZERO_FLAG)) return -1;
-    if (nes.regs.acc != 0) return -1;
-    if (nes.regs.status & NEGATIVE_FLAG) return -1;
-    if (nes.regs.status & CARRY_FLAG) return -1;
+    if (!(nes.regs.status & ZERO_FLAG)) return ZERO_SHOULD_BE_SET;
+    if (nes.regs.acc != 0) return WRONG_VALUE;
+    if (nes.regs.status & NEGATIVE_FLAG) return SIGN_SHOULD_NOT_BE_SET;
+    if (nes.regs.status & CARRY_FLAG) return CARRY_SHOULD_NOT_BE_SET;
 
     return 0;
 }
 
-int test_bit()
+// A & M == 0 -> set Z = 1
+int test_bit_1()
 {
-    struct nestor nes;
+    struct nestor nes = (struct nestor){.regs={0}};
 
     nes.regs.acc = 0x0F;
-    nes.memory[0x1111] = 0xF0;  // will set bit 7 and 6
-    absolute(&nes, bit, 0x1111);
 
-    if (!(nes.regs.status & NEGATIVE_FLAG)) return -1;
-    if (!(nes.regs.status & OVERFLOW_FLAG)) return -1;
-    if (!(nes.regs.status & ZERO_FLAG)) return -1;
+    nes.regs.pc = 0x0600;
+    nes.memory[0x0601] = 0x34; // points to memory piece to apply bit op
+    nes.memory[0x0602] = 0x12;
+    nes.memory[0x1234] = 0xF0;  // will set bit 7 and 6
+
+    absolute(&nes, bit);
+
+    if (!(nes.regs.status & NEGATIVE_FLAG)) return SIGN_SHOULD_BE_SET;
+    if (!(nes.regs.status & OVERFLOW_FLAG)) return OVERFLOW_SHOULD_BE_SET;
+    if (!(nes.regs.status & ZERO_FLAG)) return ZERO_SHOULD_BE_SET;
     
     return 0;
 }
-*/
+// A & M != 0 -> set Z = 0
+int test_bit_2()
+{
+    struct nestor nes = (struct nestor){.regs={0}};
+
+    nes.regs.acc = 0x01;
+
+    nes.regs.pc = 0x0600;
+    nes.memory[0x0601] = 0x34; // points to memory piece to apply bit op
+    nes.memory[0x0602] = 0x12;
+    nes.memory[0x1234] = 0x01;  // will set bit 7 and 6
+
+    absolute(&nes, bit);
+
+    if (nes.regs.status & NEGATIVE_FLAG) return SIGN_SHOULD_NOT_BE_SET;
+    if (nes.regs.status & OVERFLOW_FLAG) return OVERFLOW_SHOULD_NOT_BE_SET;
+    if (nes.regs.status & ZERO_FLAG) return ZERO_SHOULD_NOT_BE_SET;
+    
+    return 0;
+}
+
 
 int main()
 {
     TEST(test_immediate);
     TEST(test_absolute);
-  /*  printf( "1 %s\n", test_immediate_lda() == 0? "SUCCESS" :"FAILURE");
-    printf( "2 %s\n",  test_absolute_sta() == 0? "SUCCESS" :"FAILURE");
-    printf( "3 %s\n",  test_adc() == 0? "SUCCESS" :"FAILURE");
-    printf( "4 %s\n",  test_adc_zero() == 0? "SUCCESS" :"FAILURE");
-    printf( "5 %s\n",  test_adc_negative() == 0? "SUCCESS" :"FAILURE");
-    printf( "6 %s\n",  test_adc_overflow() == 0? "SUCCESS" :"FAILURE");
-    printf( "7 %s\n",  test_adc_carry() == 0? "SUCCESS" :"FAILURE");
-    printf( "8 %s\n",  test_and() == 0? "SUCCESS" :"FAILURE");
-    printf( "9 %s\n",  test_asl() == 0? "SUCCESS" :"FAILURE");
-    printf( "10 %s\n",  test_bit() == 0? "SUCCESS" :"FAILURE");
-*/
+    TEST(test_adc);
+    TEST(test_adc_zero);
+    TEST(test_adc_negative);
+    TEST(test_adc_overflow);
+    TEST(test_adc_carry);
+    TEST(test_and_1);
+    TEST(test_and_2);
+    TEST(test_asl_1);
+    TEST(test_asl_2);
+    TEST(test_bit_1);
+    TEST(test_bit_2);
+
+
     return 0;
 }
